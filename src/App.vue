375d0c92bc69547ec7743e08f8dc012c7fcdbc41
Rodricklp <rodickheffleylp@gmail.com>
2024-04-29 09:50:01 -0400

    Renovado VueJS v3

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+const name = "Rodrick"
+const style = "color: blue"
+</script>
+
+<template>
+  <h1>Hola {{ name.toUpperCase() }}</h1>
+  <h2 v-bind:style="style">Soy Azul</h2>
+</template>
+
+<style>
+h1 {
+  color: red;
+}
+</style>
