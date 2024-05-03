94c5d5ee4ff2f4666e19abc33687a6205cd50fd8
Rodricklp <rodickheffleylp@gmail.com>
2024-05-03 17:16:28 -0400

    Store Pinia

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -1,10 +1,24 @@
 <script setup>
+import { useRegistrarStore } from '@/modules/registrar/stores/registrarStore';
+import { ref, watch } from 'vue';
 
+const registrarStore = useRegistrarStore()
+const nombre = ref(registrarStore.nombre.value)
+const email = ref(registrarStore.email.value)
+
+watch(() => registrarStore.nombre, (newValue) => {
+    nombre.value = newValue
+})
+watch(() => registrarStore.email, (newValue) => {
+    email.value = newValue
+})
 </script>
 
 <template>
 <div class="sidebar">
-    <h2>Menú Lateral</h2>
+    <h2>Bienvenido</h2>
+    <h3>Nombre: {{ nombre }}</h3>
+    <h3>Email: {{ email }}</h3>
     <ul>
         <li><router-link to="contador">Contador</router-link></li>
         <li><router-link to="listaTareas">Lista de Tareas</router-link></li>
--- src/modules/registrar/views/RegistrarView.vue
+++ src/modules/registrar/views/RegistrarView.vue
@@ -1,8 +1,15 @@
 <script setup>
 import { Form, Field, ErrorMessage } from 'vee-validate'
 import { schema } from '../schemas/validationSchema'
+import { useRegistrarStore } from '../stores/registrarStore'
+import { ref } from 'vue'
+
+const registrarStore = useRegistrarStore()
+const nombre = ref("")
+const email = ref("")
 
 const onSubmit = () => {
+    registrarStore.guardarRegistro(nombre.value, email.value)
     console.log("Se ha enviado el formulario.")
 }
 </script>
@@ -14,13 +21,13 @@ const onSubmit = () => {
         <Form action="" :validation-schema="schema" @submit="onSubmit">
             <div class="form">
                 <label for="nombre">Nombre:</label>
-                <Field type="text" placeholder="ingrese su nombre" name="nombre" />
+                <Field type="text" placeholder="ingrese su nombre" name="nombre" v-model="nombre" />
                 <ErrorMessage name="nombre"></ErrorMessage>
             </div>
             
             <div class="form">
                 <label for="email">Email:</label>
-                <Field type="email" placeholder="ingrese su email" name="email" />
+                <Field type="email" placeholder="ingrese su email" name="email" v-model="email" />
                 <ErrorMessage name="email"></ErrorMessage>
             </div>
             <div class="form">
