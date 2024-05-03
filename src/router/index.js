061c4edab400ed4f68cc4294d421a437099b3c56
Rodricklp <rodickheffleylp@gmail.com>
2024-05-03 12:40:28 -0400

    Iniciando con vue_router

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,21 @@
-import { createMemoryHistory, createRouter } from 'vue-router'
-
-
-
-const routes = [
-]
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
-  history: createMemoryHistory(),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/primeraParte',
-      name: 'primeraParte',
-      component: () => import('../components/primeraParte.vue')
+      path: '/',
+      name: 'home',
+      component: HomeView
     },
     {
-      path: '/listaTareas',
-      name: 'listaTareas',
-      component: () => import('../components/listaTareas.vue')
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/AboutView.vue')
     }
   ]
 })
