b6f3fabc2e1b7a7ac5ce163324685ec4f7d3d18c
Rodricklp <rodickheffleylp@gmail.com>
2024-05-03 14:30:56 -0400

    Rutas Funcionando

---
+++ src/components/Contador.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { ref } from 'vue'
+
+const contador = ref(0)
+const increment = () => {
+  contador.value++
+}
+const decrement = () => {
+  contador.value--
+}
+</script>
+
+<template>
+  <h1>Hola Mundo</h1>
+  <p>{{ contador }}</p>
+  <button @click="increment">Increment</button>
+  <button @click="decrement">Decrement</button>
+</template>
+
+<style scoped>
+p {
+  color: blue;
+}
+</style>
---
+++ src/components/SideBar.vue
@@ -0,0 +1,21 @@
+<script setup>
+
+</script>
+
+<template>
+<div class="sidebar">
+    <h2>Menú Lateral</h2>
+    <ul>
+        <li><router-link to="contador">Contador</router-link></li>
+        <li><router-link to="listaTareas">Lista de Tareas</router-link></li>
+    </ul>
+</div>
+</template>
+
+<style scoped>
+.sidebar {
+    width: 200px;
+    background-color: darkolivegreen;
+    padding: 20px;
+}
+</style>
